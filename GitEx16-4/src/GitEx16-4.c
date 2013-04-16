eab8b5f7f69e9bd12baaced22d714a8e62da943e
Padraig Cunningham <padraig.cunningham@ucd.ie>
2013-04-16 10:50:01 +0100

    Commit 1

---
+++ GitEx16-4/src/GitEx16-4.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : GitEx16-4.c
+ Author      : P‡draig Cunningham
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Git Example V1"); /* prints Git Example! */
+	return EXIT_SUCCESS;
+}
